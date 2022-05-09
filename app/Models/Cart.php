45439a40925a878210db844d3be7494488084496
shirowhitte <91487648+shirowhitte@users.noreply.github.com>
2022-05-10 03:28:29 +0800

    left email feature

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,18 +8,22 @@ class Cart
     public $items = null;
     public $totalQty = 0;
     public $totalPrice = 0;
+    public $restaurant = null;
 
     public function __construct($oldCart)
     {
-        if ($oldCart) {
+        if ($oldCart) 
+        {
             $this->items = $oldCart->items;
             $this->totalQty = $oldCart->totalQty;
             $this->totalPrice = $oldCart->totalPrice;
+            $this->restaurant = $oldCart->restaurant;
         }
     }
 
-    public function add($item, $id) {
-        $storedItem = ['qty' => 0, 'pri' => $item->price, 'item' => $item];
+    public function add($item, $id) 
+    {
+        $storedItem = ['qty' => 0, 'pri' => $item->price, 'item' => $item, 'res' => $item->restaurant_id];
         if ($this->items) {
             if (array_key_exists($id, $this->items)) {
                 $storedItem = $this->items[$id];
@@ -27,15 +31,20 @@ class Cart
         }
         $storedItem['qty']++;
         $storedItem['pri'] = $item->price * $storedItem['qty'];
+        $storedItem['res'] = $item->restaurant_id; 
         $this->items[$id] = $storedItem;
         $this->totalQty++;
         $this->totalPrice += $item->price;
+        $this->restaurant =  $storedItem['res'];
     }
 
-    public function reduce($item, $id) {
+    public function reduce($item, $id) 
+    {
         $storedItem = ['qty' => 0, 'pri' => $item->price, 'item' => $item];
-        if ($this->items) {
-            if (array_key_exists($id, $this->items)) {
+        if ($this->items) 
+        {
+            if (array_key_exists($id, $this->items)) 
+            {
                 $storedItem = $this->items[$id];
             }
         }
@@ -45,12 +54,14 @@ class Cart
         $this->totalQty--;
         $this->totalPrice -= $item->price;
 
-        if ($this->items[$id]['qty'] <= 0) {
+        if ($this->items[$id]['qty'] <= 0) 
+        {
             unset($this->items[$id]);
         }
     }
 
-    public function increaseByOne($item,$id) {
+    public function increaseByOne($item,$id) 
+    {
         $storedItem = ['qty' => 0, 'pri' => $item->price, 'item' => $item];
         if ($this->items) {
             if (array_key_exists($id, $this->items)) {
@@ -62,11 +73,10 @@ class Cart
         $this->items[$id] = $storedItem;
         $this->totalQty++;
         $this->totalPrice += $item->price;
-
-     
     }
 
-    public function removeItem($id) {
+    public function removeItem($id) 
+    {
         $this->totalQty -= $this->items[$id]['qty'];
         $this->totalPrice -= $this->items[$id]['pri'];
         unset($this->items[$id]);
